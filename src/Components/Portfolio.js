b8aff725245cef2cc659f639d38854620e36a98c
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-03-20 19:32:09 -0400

    Landing Page logic

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { Container,Box } from "@mui/material";
+import React from 'react';
+import { Slide } from '@mui/material';
 
 const Portfolio = () => {
   return (
-    <Container>
-        <Box position="absolute" top={-80} left={-65}>
-      <img src="public/assets/Spinnerspic2.png" alt="Bio Background" style={{ width: "580px", height: "auto" }} />
-      </Box>
-    </Container>
+    <div style={{ height: '2000px', position: 'relative', marginTop: '1px' }}> {/* Adjust marginTop to create space for the AppHeader */}
+      <Slide direction="down" in={true} timeout={900}>
+        <img
+          src="public/assets/PORTFOLIOPG1.png"
+          alt="Portfolio Image"
+          style={{
+            width: '90%', // Adjust width to make the image smaller
+            height: 'auto', // Allow height to adjust automatically
+            objectFit: 'cover',
+            position: 'relative', // Change position to relative
+            left: '5%', // Adjust left position to move the image slightly to the right
+            transform: 'translateX(-50%)',
+          }}
+        />
+      </Slide>
+    </div>
   );
 };
-
+ 
 export default Portfolio;
