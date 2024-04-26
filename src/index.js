e8b3d31675d1731e07253ae4ad8b524077ce1134
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-04-26 18:37:36 -0400

    cont

--- src/index.js
+++ src/index.js
@@ -7,7 +7,81 @@ import LandingPage from './Components/LandingPage';
 import Home from './Home';
 
 const defaultTheme = createTheme({
-  // Your theme configuration...
+  palette: {
+    mode: 'light',
+    //Blushy Nudes
+    primary: {
+      main: '#f3dfd7',
+      light: '#f5e5df',
+      dark: '#66c4e1',
+      contrastText: '#3d3d3d',
+    },
+    //cream
+    secondary: {
+      main: '#efe19f',
+      light: '#f8f3dd',
+      dark: '#eadb99',
+    },
+    //purple
+    accentPurple: {
+      dark: '#c999ff',
+      main: '#d3adff',
+      light: '#e9d6ff',
+    },
+    //pink
+    accentPink: {
+      dark: '#FE86AE',
+      main: '#FEA0C0',
+      light: '#FEC2D6',
+      darkest: '#fd5d93'
+    },
+    //yellow
+    accentYellow: {
+      main: 'eeee73',
+    },
+    //Pale Nude Blush
+    background: {
+      default: '#faf2ef',
+    },
+    text: {
+      primary: 'rgba(20,20,20,1)',
+      secondary: 'rgba(40,40,42,0.7)',
+    },
+  },
+  typography: {
+    fontWeightLight: 200,
+    fontFamily: 'Athelas',
+    h1: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    h2: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    h3: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    h4: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    h5: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    h6: {
+      fontFamily: 'Athelasr',
+      fontWeight: 800,
+    },
+    body1: {
+      fontWeight: 550
+    },
+    body2: {
+      fontWeight: 500
+    }
+  },
 });
 
 const App = () => {
